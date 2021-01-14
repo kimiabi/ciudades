285ccc8e8d8797c81511a60a9b3cb0bf9f8db601
kimiabi <kimiabigail3@gmail.com>
2021-01-13 20:13:14 -0600

    se agregaron espacios de tarjeta

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,43 +2,49 @@
   <b-container>
     <b-row align-h="center">
       <b-col cols="12" sm="12" md="6" lg='6'>
-        <h1>Sing In</h1>
-        <b-form> 
-          <b-button type="submit" variant="primary">Fb</b-button>
-          <b-button type="reset" variant="danger">Google</b-button>
-          <b-form-group
-            id="input-group-1"
-            label="Email"
-            label-for="input-email"
-          >
-            <b-form-input
-              id="input-email"
-              type="email"
-              v-model="email"
-              placeholder="Ingresar email"
-              required
+        <b-card class="mt-5">
+          <template #header>
+          <h1>Sing In</h1>
+          </template>
+          <b-card-body>
+          <b-form> 
+            <b-button type="submit" variant="primary">Fb</b-button>
+            <b-button type="reset" variant="danger">Google</b-button>
+            <b-form-group
+              id="input-group-1"
+              label="Email"
+              label-for="input-email"
             >
-            </b-form-input>
-          </b-form-group>
-          <b-form-group
-            id="input-group-2"
-            label="Contrasena"
-            label-for="input-contrasena"
-          >
-            <b-form-input
-              id="input-contrasena"
-              v-model="password"
-              type="password"
+              <b-form-input
+                id="input-email"
+                type="email"
+                v-model="email"
+                placeholder="Ingresar email"
+                required
+              >
+              </b-form-input>
+            </b-form-group>
+            <b-form-group
+              id="input-group-2"
+              label="Contrasena"
+              label-for="input-contrasena"
+            >
+              <b-form-input
+                id="input-contrasena"
+                v-model="password"
+                type="password"
 
-              placeholder="Ingresar tu contrasena"
-              required
-            >
-            </b-form-input>
-          </b-form-group>
-          <b-button @click="autenticar" variant="primary">Ingresar</b-button>
+                placeholder="Ingresar tu contrasena"
+                required
+              >
+              </b-form-input>
+            </b-form-group>
+            <b-button @click="autenticar" variant="primary">Ingresar</b-button>   
         </b-form>
+          </b-card-body> 
+        </b-card>
       </b-col>
-    </b-row>
+    </b-row>  
   </b-container>
 </template>
 <script>
